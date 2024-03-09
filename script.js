e78f654d03ed83971bb4662086c51f7b94e7f6ed
Ankit Yadav <khushhal2112@gmail.com>
2024-03-09 11:38:07 +0000

    conatiner config done

---
+++ script.js
@@ -0,0 +1,24 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+}q
+listContainer.addEvemtListener("click",function(e){
+    if(e.target.tagname === "LI"){
+        e.target.classList.toggle("checked"); 
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
